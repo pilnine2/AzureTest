12e43831f4f24826c61bf912732110aa1cff70ac
HyungMin Kim <pilnine@gmail.com>
2018-01-02 17:36:30 -0800

    please take this commit, pilnine2

--- SpeakUp/SpeakUp/Controllers/ValuesController.cs
+++ SpeakUp/SpeakUp/Controllers/ValuesController.cs
@@ -18,7 +18,7 @@ namespace SpeakUp.Controllers
         // GET api/values/5
         public string Get(int id)
         {
-            return "value46";
+            return "value___________________________1";
         }
 
         // POST api/values
